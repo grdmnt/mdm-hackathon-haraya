332fa689418a17692f357f11b2b981669408d115
Gerard Montemayor <grbmontemayor@gmail.com>
2017-10-22 00:35:37 +0800

    Update dataset

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,7 +9,7 @@ class StaticPagesController < ApplicationController
   def dataset
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
     html = markdown.render("This is *bongos*, indeed." * 100)
-    @dataset = { name: 'Dataset Name', documentation: html, date_published: 'July 10, 2017' }
+    @dataset = { name: 'Dataset Name', documentation: html, date_published: 'July 10, 2017', author: 'Je Marie Apolinario' }
   end
 
   def new_dataset
