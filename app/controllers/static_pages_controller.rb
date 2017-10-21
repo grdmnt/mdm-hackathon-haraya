33291ba1d71cbda4b77c5611ecd4797e7423dbfc
Gerard Montemayor <grbmontemayor@gmail.com>
2017-10-21 22:39:06 +0800

    Add user icon and dataset page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,16 @@ class StaticPagesController < ApplicationController
   def login
   end
 
-  def dashboard
+  def explore
     @communities = ['transparencyPH', 'agriculture', 'medical-missions']
+  end
+
+  def dataset
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+    html = markdown.render("This is *bongos*, indeed." * 100)
+    @dataset = { name: 'Dataset Name', documentation: html, date_published: 'July 10, 2017' }
+  end
+
+  def new_dataset
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages#login'
-
-  get 'dashboard' => 'static_pages#dashboard'
-
+  root 'static_pages#explore'
+  
+  get 'dataset' => 'static_pages#dataset'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
