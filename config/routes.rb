4a6c4daf25305476f4867dd491cceabd2e4cae03
Gerard Montemayor <grbmontemayor@gmail.com>
2017-10-21 23:52:48 +0800

    Add new dataset page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#explore'
   
   get 'dataset' => 'static_pages#dataset'
+  get 'new_dataset' => 'static_pages#new_dataset'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
